8c9977decfdddfaaee1d602d19c96358c70adc4a
Yomaira Escano <yjescano@gmail.com>
2018-11-14 15:53:59 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+  get '/posts/:id/body', to: 'posts#body'
 end
